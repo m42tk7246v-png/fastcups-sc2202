e616e6cf86579545a1d6aa49de6847a43b5f0ad2
Simon Stephenson <simonstephenson@iMac.mymodem>
2026-05-13 14:25:05 +1000

    Switch to gunicorn for Render deployment

--- fastcups.py
+++ fastcups.py
@@ -56,5 +56,6 @@ def handle_disconnect():
 @patch
 def count(self:L): return len(self)
 
-socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
 
